99ff640768f7b61a4e90f35291b16e829342f04a
quocthang123 <36984966+quocthang123@users.noreply.github.com>
2021-07-12 17:32:00 +0700

    second commit

--- BE/mail.project/src/main/java/com/min/mail/project/Application.java
+++ BE/mail.project/src/main/java/com/min/mail/project/Application.java
@@ -3,11 +3,21 @@ package com.min.mail.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.min.mail.project.dto.request.LoginDto;
+import com.min.mail.project.service.LoginService;
+import com.min.mail.project.service.LoginServiceImpl;
+
 @SpringBootApplication
 public class Application {
-
+	
+	
 	public static void main(String[] args) {
+		LoginDto request = new LoginDto();
+		request.setUserName("admin");
+		request.setPassword("admin");
+		LoginService loginService =new LoginServiceImpl();
 		SpringApplication.run(Application.class, args);
+		System.out.println(loginService.login(request));
 	}
 
 }
